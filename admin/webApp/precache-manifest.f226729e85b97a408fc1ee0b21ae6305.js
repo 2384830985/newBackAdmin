7d6197c0e5e668ccf20e3d2f1e114a0ed4161ca2
tang <jinxintang@iyuedian.com>
2019-01-15 21:29:15 +0800

    添加新项目

---
+++ admin/webApp/precache-manifest.f226729e85b97a408fc1ee0b21ae6305.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "02fb17377b40c8529c963f743ddd03af",
+    "url": "index.html"
+  },
+  {
+    "revision": "49054fc84d814a58040c",
+    "url": "assets/js/chunk-vendors.74687cab.js"
+  },
+  {
+    "revision": "c191d07eff2bab9311e5",
+    "url": "assets/js/app.55770e9e.js"
+  },
+  {
+    "revision": "f3f829a724103a6f5ae9",
+    "url": "assets/js/about.2e26282d.js"
+  },
+  {
+    "revision": "9366c3ca30f0205983bd0458d9bf42c0",
+    "url": "assets/img/logo.9366c3ca.jpg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "assets/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "assets/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "49054fc84d814a58040c",
+    "url": "assets/css/chunk-vendors.870b3b90.css"
+  },
+  {
+    "revision": "c191d07eff2bab9311e5",
+    "url": "assets/css/app.88cb927b.css"
+  }
+];
